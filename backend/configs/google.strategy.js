83d28f4d6e7c0515b4949b12558228dae34bbfd8
Diogo Perdigão <diogopbritto@gmail.com>
2019-12-06 10:35:36 -0300

    Exemplo de rotas
    Login pelo google exemplo

---
+++ backend/configs/google.strategy.js
@@ -0,0 +1,38 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: '531587136969-fdh72m1o3l5j8so81aojkbkfb9m15mgf.apps.googleusercontent.com',
+			clientSecret: 'IfY-CodlKsRNJgIC_ISz91dh',
+			callbackURL: 'http://be-dev.travelrpg.com:8080/auth/google/callback'
+		},
+		function(accessToken, refreshToken, profile, done) {
+			const onError = () => {
+				console.log('Ocorreu um erro!');
+			};
+			console.warn(profile);
+			/*User.findOrCreate({ googleId: profile.id }, function(err, user) {
+				return done(err, user);
+			});*/
+			return done(undefined, profile);
+		}
+	)
+);
+
+passport.serializeUser(function(user, done) {
+	const onError = () => {
+		console.log('Ocorreu um erro!');
+	};
+
+	done(undefined, user);
+});
+
+passport.deserializeUser(function(user, done) {
+	const onError = () => {
+		console.log('Ocorreu um erro!');
+	};
+
+	done(undefined, user);
+});
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+
+var url = require('url');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	var q = url.parse(req.url, true);
+	res.render('index', { title: 'Travel RPG' });
 });
+
+/* GET home page. */
+router.get('/api', function(req, res, next) {
+	console.log('api');
+	var q = url.parse(req.url, true);
+	console.log('host ' + q.host); //returns 'localhost:8080'
+	console.log('path ' + q.pathname); //returns '/default.htm'
+	console.log('search' + q.search); //returns '?year=2017&month=february'
+
+	res.render('index', { title: 'Express' });
+});
+
+router.get('/auth/google', passport.authenticate('google', { scope: 'https://www.googleapis.com/auth/plus.login' }));
+
+router.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), function(req, res) {
+	res.redirect('/admin');
+});
+
+router.get('/admin', ensureAuthenticated, function(req, res) {
+	res.render('admin', { user: req.session.passport.user });
+});
+
+function ensureAuthenticated(req, res, next) {
+	if (req.isAuthenticated()) {
+		// req.user is available for use here
+		return next();
+	}
+	// denied. redirect to login
+	res.redirect('/');
+}
 
 module.exports = router;
