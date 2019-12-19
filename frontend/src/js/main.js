245809b60ddafaa58ba5633c64725656d1ba6a4e
pedroribeiro89 <pedroribeiro89@hotmail.com>
2019-12-19 11:09:15 -0200

    front js

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -1,27 +1,92 @@
 import '../css/normalize.css';
 import '../css/styles.scss';
 
-console.log('Hello World');
+let fileList = [];
+let table;
 
-	function loadUserData() {
-      $.ajax({
-        url: "./users", success: function(result) {
+function drawTable() {
+	let rows = '';
+	for (let i = 0; i < fileList.length; ++i) {
+		rows += `
+		<tr id=\"${fileList[i]['name']}\">
+			<td>${fileList[i]['name']}</td>
+			<td>${fileList[i]['lastModified']}</td>
+			<td>${fileList[i]['size']}</td>
+			<td>
+				<a href=\"${fileList[i]['signedUrl']}\">
+					<button class=\"btn btn-success\">
+				  		<i class=\"fa fa-download\"></i> Download
+				  	</button>
+				</a>
+              <button class=\"btn btn-danger delete\" onclick=\"deleteFile(\'${fileList[i]['name']}\')\">
+              	<i class="fa fa-trash"></i> Delete
+              </button>
+			</td>
+		</tr>`;
+	}
+	$('#fileTable tbody').empty().append(rows);
+}
+
+function loadUserData() {
+	$.ajax({
+    	url: "./users", success: function(result) {
           $('#avatar').attr('src', result['avatar']);
           $('#userName').empty().append(result['displayName']);
         }, error: function(jqXHR, status, error) { 
           console.log(jqXHR.responseText);
         }
-      });
-	}
+    });
+}
 
-	function loadTableItens() {
+function loadTableItens() {
+	$.ajax({
+    	url: "./files", success: function(result) {
+          fileList = result;
+          drawTable();
+        }, error: function(jqXHR, status, error) { 
+          console.log(jqXHR.responseText);
+        }
+    });
+}
 
-	}
+function deleteFile(fileName) {
+	$.ajax({
+    	url: './files/' + encodeURIComponent(fileName),
+        type: 'DELETE',
+        success: function (data) {
+        	//TODO apagar da tabela
+        }
+    });
+}
+
+$(".delete").click(function() {
+	deleteFile($(this).attr('data-name'));
+});
+
+$("uploadBtn").click(function() {
+	deleteFile($(this).attr('data-name'));
+});
 
 $(document).ready(
 	() => {
+		// table = $('#fileTable').DataTable();
 		loadUserData();
-		loadTableItens();
+		// loadTableItens();
+		fileList = [
+			{
+		      "name": 'arq1',
+		      "lastModified": '01/02/1989',
+		      "size": '2GB',
+		      "signedUrl": 'www.google.com'
+		    },
+		    {
+		      "name": 'arq2',
+		      "lastModified": '03/02/1989',
+		      "size": '3GB',
+		      "signedUrl": 'www.google.com'
+		    }
+		];
+        drawTable();
 	}
 );
 
